fa16a8b1466e5ce64684c051e19d085b3b0666ed
ReshaYuana <145420142+ReshaYuana@users.noreply.github.com>
2023-11-02 16:20:12 +0700

    pertemuan 5

---
+++ pertemuan5/ContohOverrideInheritance/Mobil.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pertemuan5.ContohOverrideInheritance;
+
+/**
+ *
+ * @author HP
+ */
+public class Mobil {
+
+   public void jalankan(){
+       System.out.println(" Masukkan gigi 1, lepas kopling sambil tekan gas...");
+   }
+    
+}
---
+++ pertemuan5/ContohOverrideInheritance/MobilMatic.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pertemuan5.ContohOverrideInheritance;
+
+/**
+ *
+ * @author HP
+ */
+public class MobilMatic extends Mobil{
+    
+    @Override
+    public void jalankan(){
+        System.out.println("Gigi di posisi D, tekan gas");
+    }
+}
